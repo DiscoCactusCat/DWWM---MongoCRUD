241ed07df05c81c1b35a91726d62c7bb478bb340
Caroline Roy <caroline.roy.tln@gmail.com>
2021-03-19 11:10:39 +0100

    Ajout des routes avec requetes CRUD sur contacts

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+
+const router = require('./src/routes/crmRoutes');
+router.routes(app);
+
+const PORT = 3000;
+
+app.get('/', (req,resp)=>{
+    resp.send("Express server is running on port : " + PORT);
+});
+
+app.listen(PORT, ()=>{
+    console.log('Serveur running on port', PORT);
+})
---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,30 @@
+const routes = (app) => {
+  app
+    .route("/contacts")
+    .get(
+        // Middleware : Réception + Traitement
+      (req, resp, next) => {
+        console.log("Request GET of a user on contacts/");
+        next();
+      },
+    //   // Envoi de la réponse
+      (req, resp) => {
+        resp.send("GET contacts works");
+      }
+    )
+
+    .post((req, resp) => {
+      resp.send("POST contacts works");
+    });
+
+  app
+    .route("/contacts/:id")
+    .put((req, resp) => {
+      resp.send("PUT contacts/" + req.params.id + " works");
+    })
+    .delete((req, resp) => {
+      resp.send("DELETE contacts/" + req.params.id + " works");
+    });
+};
+
+exports.routes = routes;
